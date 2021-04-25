bd40d7e8145222a3ca8a268f1e7d4c0102a30378
carlo <ecaliwanag@gmail.com>
2021-04-26 01:46:39 +0800

    add api's

---
+++ create.js
@@ -0,0 +1,24 @@
+import * as uuid from 'uuid';
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      // The attributes of the item to be created
+      userId: '123', // The id of the author
+      goatId: uuid.v1(), // A unique uuid
+      goatName: data.goatName, // Parsed from request body
+      breed: data.breed, // Parsed from request body
+      profileImage: data.profileImage,
+      goatStatus: data.goatStatus,
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return params.Item;
+});
